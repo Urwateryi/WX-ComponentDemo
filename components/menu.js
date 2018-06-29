7e11853598142b72e2ddae3a9e96fdb763085513
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-06-29 11:15:56 +0800

    添加事件

--- components/menu.js
+++ components/menu.js
@@ -8,26 +8,33 @@ Component({
   data: {
     showMenu: true
   },
-  
-  attached: function () {
+
+  attached: function() {
     this.setData({
       menu_list: this.data.menu_list
     })
   },
   methods: {
     // 点击新建按钮
-    onCreateTap: function () {
+    onCreateTap: function() {
       this.setData({
         showMenu: !this.data.showMenu
       })
     },
     // 点击展开的单个按钮
-    onItemTap: function (event) {
+    onItemTap: function(event) {
       var item = event.currentTarget.dataset.item;
-      wx.showToast({
-        title: "新建" + item.name,
-        icon: 'none'
-      })
+      // 微信小程序中是通过triggerEvent来给父组件传递信息的
+      //triggerEvent：https://developers.weixin.qq.com/miniprogram/dev/framework/custom-component/events.html
+      var menuEventDetail = {
+        item
+      }
+      this.triggerEvent('handleMenu', menuEventDetail)
+      //menuEventOption是触发事件的选项，包括设置事件是否冒泡之类的，不过这里默认是不冒泡的
+      // var menuEventOption = {
+      //   
+      // }
+      // this.triggerEvent('handleMenu', menuEventDetail, menuEventOption)
     }
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,21 @@
 var menuData = require('../../datas/menu-data.js')
+var Logger = require('../../utils/Logger.js')
 
 Page({
   onLoad: function() {
     this.setData({
       menu_list: menuData.menu_list,
+    })
+  },
+  onReady: function() {
+    this.menu = this.selectComponent("#menu");
+  },
+  handleMenu: function(event) {
+    //这里的detail就是在自定义组件中定义的menuEventDetail
+    var item = event.detail.item;
+    Logger.v("item", item);
+    wx.showToast({
+      title: '新建' + item.name,
     })
   }
 })
